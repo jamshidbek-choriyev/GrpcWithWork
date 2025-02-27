13daf2389411eac7bb28ddde7a654a4d60048500
Jamshidbek <161879703+jamshidbek7375@users.noreply.github.com>
2025-02-27 15:24:20 +0500

    Add project files.

---
+++ GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace GrpcClient;
+
+using Grpc.Net.Client;
+using GrpcCarService;
+using System.Threading.Tasks;
+
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5033");
+        var client = new CarService.CarServiceClient(channel);
+
+        //var newCar = new Car { Model = "Tesla", Color = "Red", Year = 2023 };
+        //var createdCar = await client.CreateCarAsync(newCar);
+        //Console.WriteLine($"Car created: {createdCar.Car.Model} - {createdCar.Car.Color}");
+
+        var allCars = await client.GetAllCarsAsync(new Empty());
+        Console.WriteLine("All Cars:");
+        foreach(var car in allCars.Cars)
+        {
+            Console.WriteLine($"{car.Id}: {car.Model}, {car.Color}, {car.Year}");
+        }
+
+        //var carById = await client.GetCarByIdAsync(new CarIdRequest { Id = "67beeead8937f641706120e3" });
+        //Console.WriteLine($"Car found: {carById.Car.Model}");
+
+        //var updatedCar = await client.UpdateCarAsync(new Car { Id = "67beeead8937f641706120e3", Model = "BMW", Color = "Black", Year = 2024 });
+        //Console.WriteLine($"Updated Car: {updatedCar.Car.Model}");
+
+        //await client.DeleteCarAsync(new CarIdRequest { Id = "67beeead8937f641706120e3" });
+        //Console.WriteLine("Car deleted successfully");
+    }
+}
---
+++ GrpcServer/Services/CarService.cs
@@ -0,0 +1,59 @@
+﻿namespace GrpcServer.Services;
+
+using Grpc.Core;
+using GrpcCarService;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using static GrpcCarService.CarService;
+
+public class CarService : CarServiceBase
+{
+    private readonly IMongoCollection<Car> _cars;
+    public CarService(IConfiguration config)
+    {
+        var client = new MongoClient(config["MongoDB:ConnectionString"]);
+        var database = client.GetDatabase(config["MongoDB:DatabaseName"]);
+        _cars = database.GetCollection<Car>(config["MongoDB:CollectionName"]);
+    }
+
+    public override async Task<CarResponse> CreateCar(Car request, ServerCallContext context)
+    {
+        request.Id = ObjectId.GenerateNewId().ToString();
+        await _cars.InsertOneAsync(request);
+        return await Task.FromResult(new CarResponse { Car = request });
+    }
+
+    public override Task<CarResponse> GetCarById(CarIdRequest request, ServerCallContext context)
+    {
+        var car = _cars.FindAsync(c => c.Id == request.Id).Result.FirstOrDefault();
+        if(car == null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Car not found"));
+
+        return Task.FromResult(new CarResponse { Car = car });
+    }
+
+    public override Task<CarListResponse> GetAllCars(Empty request, ServerCallContext context)
+    {
+        return Task.FromResult(new CarListResponse { Cars = { _cars.Find(_ => true).ToList() } });
+    }
+
+    public override Task<CarResponse> UpdateCar(Car request, ServerCallContext context)
+    {
+        var car = _cars.FindAsync(c => c.Id == request.Id).Result.FirstOrDefault();
+        if(car == null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Car not found"));
+
+        _cars.ReplaceOne(c => c.Id == request.Id, request);
+        return Task.FromResult(new CarResponse { Car = car });
+    }
+
+    public override Task<Empty> DeleteCar(CarIdRequest request, ServerCallContext context)
+    {
+        var car = _cars.FindAsync(c => c.Id == request.Id).Result.FirstOrDefault();
+        if(car == null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Car not found"));
+
+        _cars.DeleteMany(c => c.Id == request.Id);
+        return Task.FromResult(new Empty());
+    }
+}
